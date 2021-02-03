bcecdb22525c50c7b4bc947651c92d80e5a37d5d
Kath_code <kathmene27@gmail.com>
2021-02-03 18:57:45 -0500

    Crate a search component

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Search from './components/search/Search.svelte';
+
+  const handleSubmit = (event: CustomEvent<string>) => {
+    console.log(event.detail)
+  }
+</script>
+
+<main>
+  <Search on:submit={handleSubmit} />
+</main>
+
+<style>
+
+</style>
---
+++ src/components/search/Search.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  let searchTerm: string = '';
+  const dispatchSubmit = createEventDispatcher();
+
+  const handleSubmit = () => {
+    dispatchSubmit('submit', searchTerm)
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input placeholder="Search" bind:value={searchTerm} name="search" />
+  <input type="submit" value="Search" />
+</form>
+
+<style>
+
+</style>
